187f99ae99404bb4b061e5f845b200a8826bae34
Alesandro Silva <silva_alesandro@outlook.com>
2021-05-21 01:25:48 -0400

    final version

--- qrgenarator.js
+++ qrgenarator.js
@@ -3,12 +3,21 @@ const excel = require("xlsx");
 
 //String(42).padStart(5, '0');
 
+function createMany(prefix, lastNumber) {
+  for (i = 1; i <= lastNumber; i++) {
+    let sufix = String(i).padStart(4, '0');
+    qr.toFile(`images/BBA/${prefix}${sufix}.svg`, `${prefix}${sufix}`, {width: 110});
+  }
+}
+
 function createQr(filename, data) {
-    qr.toFile("images/" + filename + ".svg", data, { width: 400 });
+
+    qr.toFile("images/" + filename + ".svg", data, { width: 110 });
 }
 
 //const ws = excel.readFile("file.xlsx").Sheets["Página1"];
 //const data = excel.utils.sheet_to_json(ws);
 //console.log(data);
 
-createQr("qr", "BBA0001");
+//createQr("qr_150", "BBA0001");
+createMany("BBA", 600);
